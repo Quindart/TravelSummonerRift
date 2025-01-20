556ef983b6d5c6e241165cba24f181560fe97b93
tranvy57 <tranvy.art@gmail.com>
2025-01-21 01:09:19 +0700

    add: all repositories

---
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/entities/BaseEntity.java
@@ -0,0 +1,31 @@
+package vn.edu.iuh.fit.userservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+@Getter
+@Setter
+public class BaseEntity {
+    protected boolean isActive = true;
+    @Column(updatable = false)
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
