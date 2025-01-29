20afab4fc918388bbbec05e1f434b928910ec6fd
tranvy57 <tranvy.art@gmail.com>
2025-01-29 22:50:41 +0700

    add: api introspect

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/controllers/AuthController.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/controllers/AuthController.java
@@ -1,14 +1,19 @@
 package vn.edu.iuh.fit.userservice.controllers;
 
+import com.nimbusds.jose.JOSEException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vn.edu.iuh.fit.userservice.dtos.requests.AuthenticationRequest;
+import vn.edu.iuh.fit.userservice.dtos.requests.IntrospectRequest;
 import vn.edu.iuh.fit.userservice.dtos.responses.AuthenticationResponse;
+import vn.edu.iuh.fit.userservice.dtos.responses.IntrospectResponse;
+import vn.edu.iuh.fit.userservice.exception.MessageResponse;
+import vn.edu.iuh.fit.userservice.exception.SuccessEntityResponse;
 import vn.edu.iuh.fit.userservice.services.AuthenticationService;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -17,8 +22,17 @@ public class AuthController {
     private AuthenticationService authenticationService;
 
     @PostMapping("/token")
-    ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+    ResponseEntity<MessageResponse<AuthenticationResponse>> authenticate(@RequestBody AuthenticationRequest request) {
         var result = authenticationService.authenticate(request);
-        return ResponseEntity.ok(result);
+        return SuccessEntityResponse.OkResponse("Đăng nhập thành công", result);
     }
+
+    @PostMapping("/introspect")
+    ResponseEntity<MessageResponse<IntrospectResponse>> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return SuccessEntityResponse.OkResponse("Kiểm tra token thành công", result);
+    }
+
+    
 }
--- UserService/src/main/java/vn/edu/iuh/fit/userservice/controllers/UserController.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/controllers/UserController.java
@@ -2,12 +2,13 @@ package vn.edu.iuh.fit.userservice.controllers;
 
 import java.util.List;
 
+import com.nimbusds.jose.JOSEException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import vn.edu.iuh.fit.userservice.dtos.requests.IntrospectRequest;
+import vn.edu.iuh.fit.userservice.dtos.responses.IntrospectResponse;
 import vn.edu.iuh.fit.userservice.dtos.responses.UserResponse;
 import vn.edu.iuh.fit.userservice.exception.MessageResponse;
 import vn.edu.iuh.fit.userservice.exception.SuccessEntityResponse;
@@ -23,4 +24,6 @@ public class UserController {
     public ResponseEntity<MessageResponse<List<UserResponse>>> getUsers() {
         return SuccessEntityResponse.FoundResponse("Đã tìm thấy", userService.getUsers());
     }
+
+    
 }
