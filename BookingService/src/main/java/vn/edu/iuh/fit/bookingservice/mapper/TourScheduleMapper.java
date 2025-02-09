16cb9213d4372b2646977c588372fda7d292f474
Hieu Lai <ldmhieudev@gmail.com>
2025-02-09 20:28:08 +0700

    Feat: Add TourScheduleMapper

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/mapper/TourScheduleMapper.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/mapper/TourScheduleMapper.java
@@ -1,4 +1,32 @@
 package vn.edu.iuh.fit.bookingservice.mapper;
 
+import jdk.jfr.Name;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+import vn.edu.iuh.fit.bookingservice.dtos.requests.TourScheduleRequest;
+import vn.edu.iuh.fit.bookingservice.dtos.responses.TourScheduleResponse;
+import vn.edu.iuh.fit.bookingservice.entities.Tour;
+import vn.edu.iuh.fit.bookingservice.entities.TourSchedule;
+
+@Mapper(componentModel = "spring")
 public interface TourScheduleMapper {
+    //    Entity to ResponseDTO
+    @Mapping(source = "tour.tourId",target = "tourId")
+    TourScheduleResponse entityToResponse (TourSchedule tourSchedule);
+    //    Request to Entity
+    @Mapping(source = "tourId", target = "tour", qualifiedByName = "IdToTour")
+    TourSchedule requestToEntity (TourScheduleRequest tourScheduleRequest);
+
+//    @Named("TourToId")
+//    default String TourToId(Tour tour){
+//        return tour != null?tour.getTourId():null;
+//    }
+    @Named("IdToTour")
+    default Tour IdToTour(String tourId){
+        Tour tour = new Tour();
+        tour.setTourId(tourId);
+        return  tour;
+    }
 }
