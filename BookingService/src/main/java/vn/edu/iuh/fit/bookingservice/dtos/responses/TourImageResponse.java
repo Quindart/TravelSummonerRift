1cad90451bc553e45f474a5970ef520888f5b840
GiaHuyIUH <tranhuyzaza@gmail.com>
2025-02-20 00:41:39 +0700

    create TourImageREsponse

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/dtos/responses/TourImageResponse.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/dtos/responses/TourImageResponse.java
@@ -1,2 +1,14 @@
-package vn.edu.iuh.fit.bookingservice.dtos.responses;public class TourImageResponse {
+package vn.edu.iuh.fit.bookingservice.dtos.responses;
+
+import lombok.Data;
+
+@Data
+public class TourImageResponse {
+    private String tourImageId;
+    private String tourId;
+    private String imageUrl;
+    private String description;
+    private int orderIndex;
+    private boolean isActive;
+
 }
