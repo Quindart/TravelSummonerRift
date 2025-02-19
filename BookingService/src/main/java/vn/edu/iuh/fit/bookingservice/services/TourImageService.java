0d09091047df66da88b073897a0937ca484bbea5
GiaHuyIUH <tranhuyzaza@gmail.com>
2025-02-20 00:43:29 +0700

    create TourImageService

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/TourImageService.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/TourImageService.java
@@ -1,2 +1,14 @@
-package vn.edu.iuh.fit.bookingservice.services;public interface TourImageService {
+package vn.edu.iuh.fit.bookingservice.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import vn.edu.iuh.fit.bookingservice.dtos.requests.TourImageRequest;
+import vn.edu.iuh.fit.bookingservice.dtos.responses.TourImageResponse;
+
+import java.io.IOException;
+
+public interface TourImageService {
+    TourImageResponse saveTourImage(MultipartFile file, TourImageRequest tourImageRequest);
+    TourImageResponse updateTourImage(String tourImageId, TourImageRequest tourImageRequest);
+    void deleteTourImage(String tourImageId) throws IOException;
 }
