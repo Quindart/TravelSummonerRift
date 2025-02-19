c089ef705f5b9254cdd7793cfd5cf0b284a00f89
GiaHuyIUH <tranhuyzaza@gmail.com>
2025-02-20 00:40:28 +0700

    create TourImage entity

---
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/controllers/TourImageController.java
@@ -0,0 +1,2 @@
+package vn.edu.iuh.fit.bookingservice.controllers;public class TourImageController {
+}
---
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/dtos/responses/TourImageResponse.java
@@ -0,0 +1,2 @@
+package vn.edu.iuh.fit.bookingservice.dtos.responses;public class TourImageResponse {
+}
