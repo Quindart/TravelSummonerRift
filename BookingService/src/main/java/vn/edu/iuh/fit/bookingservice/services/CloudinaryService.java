1d024a2bc551e2133bdfe3c93e3450377eaa21b7
GiaHuyIUH <tranhuyzaza@gmail.com>
2025-02-20 00:43:01 +0700

    create CoudinaryService

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/CloudinaryService.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/CloudinaryService.java
@@ -1,2 +1,32 @@
-package vn.edu.iuh.fit.bookingservice.services;public class CloudinaryService {
+package vn.edu.iuh.fit.bookingservice.services;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService() {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "dwip7dplu",
+                "api_key", "944366892271352",
+                "api_secret", "SGhlTmcznfxn-1T0T8D2BQQOA9Q"
+        ));
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        return uploadResult.get("url").toString();
+    }
+
+    public void deleteImage(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
 }
+
