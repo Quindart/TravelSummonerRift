4965f7124f12b542c4e82a13e39e3bfeec9e6a53
tranvy57 <tranvy.art@gmail.com>
2025-02-21 23:33:01 +0700

    add: api get my-info

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/services/AuthenticationService.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/services/AuthenticationService.java
@@ -139,7 +139,7 @@ public class AuthenticationService {
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                .subject(user.getUserId())
+                .subject(user.getUsername())
                 .issuer("zycute")
                 .issueTime(new Date())
                 .expirationTime(new Date(
