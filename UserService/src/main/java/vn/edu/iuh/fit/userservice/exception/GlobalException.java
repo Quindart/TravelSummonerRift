1834a8edda9011d2823e915dd3f7681e38bca0ca
tranvy57 <tranvy.art@gmail.com>
2025-02-21 23:11:25 +0700

    add: api register

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/exception/GlobalException.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/exception/GlobalException.java
@@ -2,6 +2,7 @@ package vn.edu.iuh.fit.userservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -63,5 +64,12 @@ public class GlobalException {
     public ResponseEntity<?> handleMaxSizeException(MaxUploadSizeExceededException exc) {
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
                 .body(new MessageResponse(413, "Kích thước file vượt quá giới hạn cho phép", false));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageDto> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        String enumKey = exception.getFieldError().getDefaultMessage();
+        ErrorMessageDto errorDto = new ErrorMessageDto(HttpStatus.BAD_REQUEST.value(), enumKey, false);
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 }
