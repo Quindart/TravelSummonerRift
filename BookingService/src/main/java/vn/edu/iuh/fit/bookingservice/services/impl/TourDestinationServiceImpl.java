0e4a1f150e42c34ba77e1796ec0fd71c8ff1f540
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-03-01 18:52:07 +0700

    ADD: TourDestinationMapper, tourDestinationRepository, TourDestinationService

---
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/impl/TourDestinationServiceImpl.java
@@ -0,0 +1,53 @@
+package vn.edu.iuh.fit.bookingservice.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.edu.iuh.fit.bookingservice.dtos.requests.TourDestinationRequest;
+import vn.edu.iuh.fit.bookingservice.dtos.responses.TourDestinationResponse;
+import vn.edu.iuh.fit.bookingservice.entities.TourDestination;
+import vn.edu.iuh.fit.bookingservice.mapper.TourDestinationMapper;
+import vn.edu.iuh.fit.bookingservice.repositories.TourDestinationRepository;
+import vn.edu.iuh.fit.bookingservice.services.TourDestinationService;
+
+import java.util.List;
+
+@Service
+public class TourDestinationServiceImpl implements TourDestinationService {
+
+    @Autowired
+    private TourDestinationRepository tourDestinationRepository;
+    @Autowired
+    private TourDestinationMapper tourDestinationMapper;
+
+    @Override
+    public List<TourDestinationResponse> getAllTourDestination() {
+        return tourDestinationRepository.findAll()
+                .stream().map(tourDestinationMapper::toTourDestinationResponse)
+                .toList();
+    }
+
+    @Override
+    public TourDestinationResponse getTourDestinationById(String tourDestinationId) {
+        return tourDestinationRepository.findById(tourDestinationId)
+                .map(tourDestinationMapper::toTourDestinationResponse)
+                .orElse(null);
+    }
+
+    @Override
+    public TourDestinationResponse createTourDestination(TourDestinationRequest tourDestinationRequest) {
+        TourDestination tourDestination =  tourDestinationMapper.toTourDestination(tourDestinationRequest);
+        TourDestination savedTourDestination = tourDestinationRepository.save(tourDestination);
+        return tourDestinationMapper.toTourDestinationResponse(savedTourDestination);
+    }
+
+    @Override
+    public TourDestinationResponse updateTourDestination(String tourDestinationId, TourDestinationRequest tourDestination) {
+        return null;
+    }
+
+    @Override
+    public void deleteTourDestination(String tourDestinationId) {
+
+    }
+
+}
