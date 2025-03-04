426dd15bfaaee8bcdd4c789d7b78892e6e7b9fbe
zy <tranvy.art@gmail.com>
2025-03-04 12:00:57 +0700

    fix: delete admin enpoints -> use @PreAuthorize to catch Access Denied exception

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/configs/SecurityConfig.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/configs/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(
                 request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                         .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS).permitAll()
-                        .requestMatchers(HttpMethod.GET, ADMIN_ENDPOINTS).hasRole("ADMIN")
+//                        .requestMatchers(HttpMethod.GET, ADMIN_ENDPOINTS).hasRole("ADMIN")
                         .anyRequest().permitAll());
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
