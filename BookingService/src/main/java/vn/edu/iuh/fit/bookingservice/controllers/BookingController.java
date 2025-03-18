b9d6fa6748f66b708adbc37e25ed733aeda14cea
zy <tranvy.art@gmail.com>
2025-03-18 13:31:30 +0700

    add: init booking  v1

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/controllers/BookingController.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/controllers/BookingController.java
@@ -2,10 +2,10 @@ package vn.edu.iuh.fit.bookingservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vn.edu.iuh.fit.bookingservice.dtos.requests.BookingRequest;
 import vn.edu.iuh.fit.bookingservice.dtos.responses.BookingResponse;
+import vn.edu.iuh.fit.bookingservice.dtos.responses.BookingResponseDTO;
 import vn.edu.iuh.fit.bookingservice.exception.MessageResponse;
 import vn.edu.iuh.fit.bookingservice.exception.SuccessEntityResponse;
 import vn.edu.iuh.fit.bookingservice.services.BookingService;
@@ -26,6 +26,17 @@ public class BookingController {
     @GetMapping("/open-feign")
     public List<BookingResponse> getBookingsByUserId() {
         return bookingService.findBookingsByUserId("34ba8507-2cea-411f-84dc-9aa236ae804e").stream().toList();
+    }
+
+    @PostMapping("create-booking")
+    public MessageResponse<BookingResponseDTO> createBooking (@RequestBody BookingRequest request){
+        BookingResponseDTO response = bookingService.createBooking(request);
+        return MessageResponse.<BookingResponseDTO>builder()
+                .statusCode(200)
+                .success(true)
+                .data(response)
+                .message("Dat tour thanh cong")
+                .build();
     }
 
 
