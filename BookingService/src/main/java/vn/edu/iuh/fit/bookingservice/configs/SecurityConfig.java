9183f905970504de4114e351ab7a4863d9496fb6
zy <tranvy.art@gmail.com>
2025-04-02 15:04:16 +0700

    add: swagger booking

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/configs/SecurityConfig.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/configs/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(
                 request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
                         .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS).permitAll()
-//                        .requestMatchers(HttpMethod.GET, ADMIN_ENDPOINTS).hasRole("ADMIN")
+                        .requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()  );
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
