cd4efdf7caee7a818f32aa01a734758d3b4b5c52
zy <tranvy.art@gmail.com>
2025-04-18 21:31:16 +0700

    fix: add env to Iclient

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/Infra/client/IUserClient.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/Infra/client/IUserClient.java
@@ -1,5 +1,4 @@
 package vn.edu.iuh.fit.bookingservice.Infra.client;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import vn.edu.iuh.fit.bookingservice.configs.AuthenticationRequestInterceptor;
 import vn.edu.iuh.fit.bookingservice.dtos.responses.UserResponse;
 
-@FeignClient(name = "user-service-client", contextId = "iUserClient", url = "http://localhost:5723/user-service", configuration = {AuthenticationRequestInterceptor.class})
+@FeignClient(name = "user-service-client", contextId = "iUserClient", url = "${app.user-client-url}", configuration = {AuthenticationRequestInterceptor.class})
 public interface IUserClient {
+
+
 
     @GetMapping("/users/{id}")
     ResponseEntity<UserResponse> getUserById(@PathVariable("id") String id);
