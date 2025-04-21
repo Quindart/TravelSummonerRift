8cf61c3c891d5a89b821de60ce8fc0bcce373756
Hieu Lai <ldmhieudev@gmail.com>
2025-04-21 16:22:30 +0700

    fix: just return user isActive = true

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/repositories/UserRepository.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package vn.edu.iuh.fit.userservice.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     Optional<User> findByEmail(String username);
+    Optional<List<User>> findByIsActive(Boolean isActive);
 }
