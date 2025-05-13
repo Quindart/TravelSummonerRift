5bfe45eace57c4f65c80e882f49ba22b67e3adc9
zy <tranvy.art@gmail.com>
2025-05-13 22:58:44 +0700

    update: add category

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/dtos/responses/TourResponse.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/dtos/responses/TourResponse.java
@@ -3,6 +3,7 @@ package vn.edu.iuh.fit.bookingservice.dtos.responses;
 import lombok.Data;
 import vn.edu.iuh.fit.bookingservice.dtos.requests.TourDestinationRequest;
 import vn.edu.iuh.fit.bookingservice.dtos.requests.TourImageRequest;
+import vn.edu.iuh.fit.bookingservice.entities.CategoryTour;
 import vn.edu.iuh.fit.bookingservice.entities.TourSchedule;
 
 import java.util.List;
@@ -19,4 +20,5 @@ public class TourResponse {
     private List<TourImageResponse> tourImageResponses;
     private List<TourScheduleResponse> tourScheduleResponses;
     private boolean isActive;
+    private CategoryTour categoryTour;
 }
