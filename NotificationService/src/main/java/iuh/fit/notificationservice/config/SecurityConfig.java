ab0a432ec4351406244df1a9a6efd516bb3462fa
Hieu Lai <ldmhieudev@gmail.com>
2025-05-17 00:11:36 +0700

    feat: spring security test

--- NotificationService/src/main/java/iuh/fit/notificationservice/config/SecurityConfig.java
+++ NotificationService/src/main/java/iuh/fit/notificationservice/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package iuh.fit.notificationservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
 public class SecurityConfig {
+    public static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
