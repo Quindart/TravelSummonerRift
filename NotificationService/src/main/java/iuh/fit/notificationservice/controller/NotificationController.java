c067ebf4160084325bb7a0721443a0ccadf00e01
Hieu Lai <ldmhieudev@gmail.com>
2025-05-17 00:12:48 +0700

    test: jackson and lombok convert

--- NotificationService/src/main/java/iuh/fit/notificationservice/controller/NotificationController.java
+++ NotificationService/src/main/java/iuh/fit/notificationservice/controller/NotificationController.java
@@ -1,4 +1,17 @@
 package iuh.fit.notificationservice.controller;
 
+import iuh.fit.notificationservice.entity.Notification;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("notification")
 public class NotificationController {
+    @GetMapping("/djtmemay")
+    public String hello(){
+        return "DJTMEMAY SPRING";
+    }
+    @PostMapping(value = "/djtmemay")
+    public Notification hello(@RequestBody Notification data){
+        return data;
+    }
 }
