833b512c209d299d69e83ef5e956ff9fde6bfe88
Hieu Lai <ldmhieudev@gmail.com>
2025-05-20 16:43:21 +0700

    feat: implement update tour schedule functionality and add IServiceMail interface

--- BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/TourScheduleService.java
+++ BookingService/src/main/java/vn/edu/iuh/fit/bookingservice/services/TourScheduleService.java
@@ -1,6 +1,8 @@
 package vn.edu.iuh.fit.bookingservice.services;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import vn.edu.iuh.fit.bookingservice.dtos.requests.TourScheduleRequest;
 import vn.edu.iuh.fit.bookingservice.dtos.responses.TourScheduleResponse;
 import vn.edu.iuh.fit.bookingservice.entities.TourSchedule;
@@ -11,7 +13,7 @@ public interface TourScheduleService {
     List<TourScheduleResponse> getAllTourSchedules();
     TourScheduleResponse getTourScheduleById(String id);
     TourScheduleResponse createTourSchedule(TourScheduleRequest tourScheduleRequest);
-    TourScheduleResponse updateTourScheduleById();
+    TourScheduleResponse updateTourSchedule( TourScheduleRequest tourScheduleRequestDTO);
     boolean deleteTourScheduleById(String idTourSchedule);
     boolean deleteAllTicketByTourScheduleId();
 }
