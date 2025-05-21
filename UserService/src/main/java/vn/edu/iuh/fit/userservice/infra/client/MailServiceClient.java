40a35ee805484ab419d9e63655ac52b7a689ad06
Hieu Lai <ldmhieudev@gmail.com>
2025-05-21 09:28:12 +0700

    chore: add open feign notification-service to user-service

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/infra/client/MailServiceClient.java
+++ UserService/src/main/java/vn/edu/iuh/fit/userservice/infra/client/MailServiceClient.java
@@ -1,14 +1,15 @@
-package vn.edu.iuh.fit.bookingservice.repositories.httpclient;
+package vn.edu.iuh.fit.userservice.infra.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import vn.edu.iuh.fit.bookingservice.configs.AuthenticationRequestInterceptor;
-import vn.edu.iuh.fit.bookingservice.dtos.requests.NotificationRequest;
-import vn.edu.iuh.fit.bookingservice.exception.MessageResponse;
+import vn.edu.iuh.fit.userservice.configs.AuthenticationRequestInterceptor;
+import vn.edu.iuh.fit.userservice.dtos.requests.NotificationRequest;
+import vn.edu.iuh.fit.userservice.dtos.requests.SendOtp;
+import vn.edu.iuh.fit.userservice.exception.MessageResponse;
 
 @FeignClient(name = "notification-service-client", contextId = "notificationClient", url = "http://localhost:2005/notification-service", configuration = {AuthenticationRequestInterceptor.class})
 public interface MailServiceClient {
     @PostMapping("/sendMail/send-mail")
-    MessageResponse<Boolean> sendMailOtp(@RequestBody()NotificationRequest notificationRequest);
+    MessageResponse<Boolean> sendMailOtp(@RequestBody() SendOtp sendOtp);
 }
